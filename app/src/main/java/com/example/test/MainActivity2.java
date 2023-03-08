091f506d882370fd89f103dfac463b12a785ad28
muktachanda <muktachanda03@gmail.com>
2023-03-08 17:30:51 +0530

    Added Recording and Results Page

--- app/src/main/java/com/example/test/MainActivity2.java
+++ app/src/main/java/com/example/test/MainActivity2.java
@@ -1,30 +1,33 @@
 package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.text.TextWatcher;
 import android.text.Editable;
-import android.content.Intent;
 import android.view.View;
-import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity2 extends AppCompatActivity {
     private EditText editTextFirstName, editTextLastName, editTextAge, editTextHeightFeet, editTextHeightInches, editTextWeight;
-    private TextInputLayout weightLayout;
     private TextView textViewBMIResult;
-    private RadioGroup radioGroupGender;
+    RadioGroup radioGroupGender;
+    Button buttonSubmit;
 
     private String firstName;
     private String lastName;
     private int age;
     private String gender;
-    private int heightFeet;
-    private int heightInches;
+    private float heightFeet;
+    private float heightInches;
     private float weight;
     private float bmi;
 
@@ -37,14 +40,31 @@ public class MainActivity2 extends AppCompatActivity {
         editTextHeightFeet = findViewById(R.id.editTextHeightFeet);
         editTextHeightInches = findViewById(R.id.editTextHeightInches);
         editTextWeight = findViewById(R.id.editTextWeight);
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextAge = findViewById(R.id.editTextAge);
+
 
         // Initialize TextView field
         textViewBMIResult = findViewById(R.id.textViewBMIResult);
+
+        // Initialize Button
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+
+        // Set the initial state of the button
+        buttonSubmit.setEnabled(false);
 
         // Add onTextChanged listeners
         editTextHeightFeet.addTextChangedListener(textWatcher);
         editTextHeightInches.addTextChangedListener(textWatcher);
         editTextWeight.addTextChangedListener(textWatcher);
+
+        editTextFirstName.addTextChangedListener(checkFields);
+        editTextLastName.addTextChangedListener(checkFields);
+        editTextAge.addTextChangedListener(checkFields);
+        editTextHeightFeet.addTextChangedListener(checkFields);
+        editTextHeightInches.addTextChangedListener(checkFields);
+        editTextWeight.addTextChangedListener(checkFields);
     }
 
     private TextWatcher textWatcher = new TextWatcher() {
@@ -53,11 +73,36 @@ public class MainActivity2 extends AppCompatActivity {
         }
 
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            calculateBMI();
+            // Do nothing
         }
 
         public void afterTextChanged(Editable s) {
-            // Do nothing
+            calculateBMI();
+        }
+    };
+
+    private TextWatcher checkFields = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            boolean anyEmpty = TextUtils.isEmpty(editTextAge.getText())
+                    || TextUtils.isEmpty(editTextWeight.getText())
+                    || TextUtils.isEmpty(editTextHeightFeet.getText())
+                    || TextUtils.isEmpty(editTextHeightInches.getText())
+                    || TextUtils.isEmpty(editTextFirstName.getText())
+                    || TextUtils.isEmpty(editTextLastName.getText());
+            radioGroupGender = findViewById(R.id.radioGroupGender);
+            boolean genderSelected = radioGroupGender.getCheckedRadioButtonId() != -1;
+            buttonSubmit.setEnabled(!anyEmpty && genderSelected);
         }
     };
 
@@ -72,9 +117,9 @@ public class MainActivity2 extends AppCompatActivity {
         String weightStr = weightEditText.getText().toString();
 
         // Convert the height and weight strings to float values
-        float heightFeet = heightFeetStr.isEmpty() ? 0 : Float.parseFloat(heightFeetStr);
-        float heightInches = heightInchesStr.isEmpty() ? 0 : Float.parseFloat(heightInchesStr);
-        float weight = weightStr.isEmpty() ? 0 : Float.parseFloat(weightStr);
+        heightFeet = heightFeetStr.isEmpty() ? 0 : Float.parseFloat(heightFeetStr);
+        heightInches = heightInchesStr.isEmpty() ? 0 : Float.parseFloat(heightInchesStr);
+        weight = weightStr.isEmpty() ? 0 : Float.parseFloat(weightStr);
 
         // Convert the height values from feet and inches to meters
         float heightMeters = (heightFeet * 0.3048f) + (heightInches * 0.0254f);
@@ -83,27 +128,40 @@ public class MainActivity2 extends AppCompatActivity {
         bmi = weight / (heightMeters * heightMeters);
 
         // Display the BMI result in the textViewBMIResult
-        TextView bmiResultTextView = findViewById(R.id.textViewBMIResult);
-        bmiResultTextView.setText("BMI: " + String.format("%.2f", bmi));
+        textViewBMIResult = findViewById(R.id.textViewBMIResult);
+        textViewBMIResult.setText("BMI: " + String.format("%.2f", bmi));
     }
 
     public void submitForm(View view) {
-        // Get the values from the EditText fields
-        firstName = editTextFirstName.getText().toString();
-        lastName = editTextLastName.getText().toString();
-        age = Integer.parseInt(editTextAge.getText().toString());
+        if (!buttonSubmit.isEnabled()) {
+            return;
+        }
+        else {
+            // Get the values from the EditText fields
+            editTextFirstName = findViewById(R.id.editTextFirstName);
+            editTextLastName = findViewById(R.id.editTextLastName);
+            editTextAge = findViewById(R.id.editTextAge);
 
-        // Get the selected radio button from the radio group
-        int selectedId = radioGroupGender.getCheckedRadioButtonId();
-        RadioButton radioButton = findViewById(selectedId);
-        gender = radioButton.getText().toString();
+            firstName = editTextFirstName.getText().toString();
+            lastName = editTextLastName.getText().toString();
+            age = Integer.parseInt(editTextAge.getText().toString());
 
-        heightFeet = Integer.parseInt(editTextHeightFeet.getText().toString());
-        heightInches = Integer.parseInt(editTextHeightInches.getText().toString());
-        weight = Float.parseFloat(editTextWeight.getText().toString());
+            // Get the radio group gender
+            radioGroupGender = findViewById(R.id.radioGroupGender);
+            int selectedId = radioGroupGender.getCheckedRadioButtonId();
+            if (selectedId != -1) {
+                RadioButton radioButton = findViewById(selectedId);
+                gender = radioButton.getText().toString();
+            }
 
-        Intent intent = new Intent(this, MainActivity3.class);
-        startActivity(intent);
+            heightFeet = Integer.parseInt(editTextHeightFeet.getText().toString());
+            heightInches = Integer.parseInt(editTextHeightInches.getText().toString());
+            weight = Float.parseFloat(editTextWeight.getText().toString());
+
+
+            startActivity(new Intent(MainActivity2.this, MainActivity3.class));
+            finish();
+        }
     }
 
 
